06bd9f95b9768cba99ab2bdd9e0ada8cfcfbfc01
username-is5 <abhi135790ko@gmail.com>
2025-08-13 15:44:57 +0530

    Configured and established connection to MongoDB atlas

---
+++ Backend/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const mongoURI = process.env.MONGO_URI;
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to Mongo Successfully");
+    } catch (error) {
+        console.error("Error connecting to Mongo:", error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectToMongo;
---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+const connectToMongo = require('./db');
+const express = require('express')
+var cors = require('cors') 
+
+connectToMongo();
+const app = express()
+const port = 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`NotesKeeper backend listening at http://localhost:${port}`)
+})
