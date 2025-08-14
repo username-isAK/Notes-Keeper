f80696162119cd4067f18223843862e647e06c01
username-is5 <abhi135790ko@gmail.com>
2025-08-14 13:19:08 +0530

    Implement user registration with JWT authentication

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express')
 var cors = require('cors') 
+require('dotenv').config();
 
 connectToMongo();
 const app = express()
@@ -8,6 +9,8 @@ const port = 5000
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/auth', require('./routes/auth'))
 
 app.listen(port, () => {
   console.log(`NotesKeeper backend listening at http://localhost:${port}`)
---
+++ Backend/routes/auth.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser');
+
+const JWT_SECRET = 'N0te$kEEpe#r';
+
+router.post('/createuser', [
+  body('name', 'Enter a valid name').isLength({ min: 3 }),
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: "Sorry a user with this email already exists" })
+    }
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+
+    user = await User.create({
+      name: req.body.name,
+      password: secPass,
+      email: req.body.email,
+    });
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+
+    res.json({ authtoken })
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
+
+module.exports = router
