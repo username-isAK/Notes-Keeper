57b3b01309aa3376a2077201145ec04606d03435
username-is5 <abhi135790ko@gmail.com>
2025-08-15 20:45:29 +0530

    Endpoints to create and read notes

--- Backend/index.js
+++ Backend/index.js
@@ -11,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/note'))
 
 app.listen(port, () => {
   console.log(`NotesKeeper backend listening at http://localhost:${port}`)
