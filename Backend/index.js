b26a6f85091f4231b58bfac21cc1725cb9a62f50
username-is5 <abhi135790ko@gmail.com>
2025-08-25 20:16:40 +0530

    Ignore this commit

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://notes-keeper-sec.vercel.app'],
     methods: ['GET','POST','PUT','DELETE'],
     credentials: true
 }));
